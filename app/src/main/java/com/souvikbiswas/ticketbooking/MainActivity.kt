3a989905b7f1db5d42bfa6c4e980b8cfcdba9fef
Souvik Biswas <sbis1999@gmail.com>
2020-08-21 19:41:37 +0530

    Add date selector

--- app/src/main/java/com/souvikbiswas/ticketbooking/MainActivity.kt
+++ app/src/main/java/com/souvikbiswas/ticketbooking/MainActivity.kt
@@ -23,6 +23,14 @@ class MainActivity : AppCompatActivity() {
     private lateinit var movieRating: TextView
     private lateinit var descriptionButton: ImageButton
 
+    private lateinit var day1: TextView
+    private lateinit var day2: TextView
+    private lateinit var day3: TextView
+    private lateinit var day4: TextView
+    private lateinit var day5: TextView
+    private lateinit var day6: TextView
+    private lateinit var day7: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -42,6 +50,14 @@ class MainActivity : AppCompatActivity() {
         movieRating = rating
         descriptionButton = description_button
 
+        day1 = day_1
+        day2 = day_2
+        day3 = day_3
+        day4 = day_4
+        day5 = day_5
+        day6 = day_6
+        day7 = day_7
+
         var isCoverView = false
         var isDescriptionView = false
 
@@ -53,6 +69,26 @@ class MainActivity : AppCompatActivity() {
 
         val descriptionConstraint = ConstraintSet()
         descriptionConstraint.clone(this, R.layout.description_view)
+
+        val mapOfDays: Map<TextView, TextView> = mapOf(
+            day1 to date_1,
+            day2 to date_2,
+            day3 to date_3,
+            day4 to date_4,
+            day5 to date_5,
+            day6 to date_6,
+            day7 to date_7
+        )
+
+        val days: List<TextView> = listOf(day1, day2, day3, day4, day5, day6, day7)
+
+        for (day in days) {
+            day.setOnClickListener { selectDate(it as TextView, descriptionConstraint) }
+        }
+
+        for (day in mapOfDays) {
+            day.value.setOnClickListener { selectDate(day.key, descriptionConstraint) }
+        }
 
         coverImage.setOnClickListener {
             if (!isCoverView) {
@@ -137,5 +173,22 @@ class MainActivity : AppCompatActivity() {
                 isDescriptionView = true
             }
         }
+    }
+
+    private fun selectDate(day: TextView, destinationConstraint: ConstraintSet) {
+        destinationConstraint.connect(
+            date_selector.id,
+            ConstraintSet.START,
+            day.id,
+            ConstraintSet.START
+        )
+        destinationConstraint.connect(
+            date_selector.id,
+            ConstraintSet.END,
+            day.id,
+            ConstraintSet.END
+        )
+        TransitionManager.beginDelayedTransition(root)
+        destinationConstraint.applyTo(root)
     }
 }
